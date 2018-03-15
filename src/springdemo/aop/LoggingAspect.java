14357cc0e70b983d4b44c3e4bb758e9edbf6f131
KrzysztofGrzywinski <krzysiekg94@gmail.com>
2018-03-15 21:24:14 +0100

    AOP logging
    Added AOP logging into console

---
+++ src/springdemo/aop/LoggingAspect.java
@@ -0,0 +1,45 @@
+package springdemo.aop;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+	private Logger logger = Logger.getLogger(getClass().getName());
+
+	@Pointcut("execution(* springdemo.controller.*.*(..))")
+	public void controllerPackage() {
+	}
+
+	@Pointcut("execution(* springdemo.service.*.*(..))")
+	public void servicePackage() {
+	}
+
+	@Pointcut("execution(* springdemo.dao.*.*(..))")
+	public void daoPackage() {
+	}
+
+	@Pointcut("controllerPackage() || servicePackage() || daoPackage()")
+	public void allServices() {
+	}
+
+	@Before("allServices()")
+	public void before(JoinPoint joinpoint) {
+
+		String method = joinpoint.getSignature().toShortString();
+		logger.info("@Before: Method called: " + method);
+
+		Object[] arguments = joinpoint.getArgs();
+
+		for (Object temp : arguments) {
+			logger.info("Argument: " + temp);
+		}
+	}
+}
--- src/springdemo/controller/CustomerController.java
+++ src/springdemo/controller/CustomerController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-
 import springdemo.entity.Customer;
 import springdemo.service.CustomerService;
 
